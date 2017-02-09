2cd2eb7a13950aeb6e5a33060f26b3779a5814a5
jconsigliere <jconsigliere@gmx.com>
2017-02-09 09:12:20 +0000

    Feat(model): destroy cloudinary image on person destruction

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,13 @@
 class Person < ApplicationRecord
   validates :full_name, presence: true
+
+  before_destroy :destroy_cloudinary_image
+
+  protected
+
+  def destroy_cloudinary_image
+    if photo_url.present?
+      Cloudinary::Uploader.destroy photo_url.split('/').last.split('.').first, invalidate: true
+    end
+  end
 end
